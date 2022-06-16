a4031de531651d531709b3ff381facf9479f3a4d
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 21:09:57 -0300

    Add files via upload

---
+++ 57-AlmacenarSumaDeVariablesIntroducidas/AlmacenarSumaDeVariablesIntroducidas/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AlmacenarSumaDeVariablesIntroducidas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Suma = 0;
+            Console.CursorLeft = 30;
+
+            Console.WriteLine("Ingresar la cantidad de valores a sumar:");
+            int CantidadDeValores = Convert.ToInt32(Console.ReadLine());
+
+            int[] SumaDeValores = new int[CantidadDeValores];
+
+            Console.WriteLine("Ingrese los valores a sumar:");
+
+            for (int i = 0; i<CantidadDeValores; i++)
+            {
+                SumaDeValores[i] = Convert.ToInt32(Console.ReadLine());
+
+                Suma = SumaDeValores.Sum();
+            }
+
+            Console.WriteLine("La suman de los valores da como resultado: " + Suma);
+
+            Console.ReadKey();
+        }
+    }
+}
